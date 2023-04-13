eca8cb9988a8142ae20eb974865147a79bb9d658
Albertus <albertuscifo@gmail.com>
2023-04-13 20:51:18 +0200

    #2 CRUD Person

--- src/main/java/com/example/carsProject/controller/PersonController.java
+++ src/main/java/com/example/carsProject/controller/PersonController.java
@@ -5,8 +5,7 @@ import com.example.carsProject.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class PersonController {
@@ -25,7 +24,46 @@ public class PersonController {
 
         Person personCreated = personService.addPerson(person);
         model.addAttribute("person", personCreated);
-        //TODO: add return template once you've created the person
+
         return "redirect:home";
     }
+
+    @RequestMapping("/filledUserForm")
+    public String returnUserForm(@RequestParam("idFromView") String id, Model model){
+        Person person = personService.getClientByKey(id);
+        model.addAttribute("person", person);
+        return "filledUserForm";
+
+    }
+
+    @RequestMapping("deleteUser")
+    public String deleteUser(@RequestParam("idFromView") String id) {
+        personService.deleteClient(id);
+        return "redirect:home";
+    }
+
+    @RequestMapping("/getUserFormToUpdate")
+    public String personUpdate(@RequestParam("idFromView") String id, Model model) {
+
+        Person personFound = personService.getClientByKey(id);
+
+        if(personFound != null){
+            model.addAttribute("userIdFromView", id);
+            model.addAttribute("person", personFound);
+            model.addAttribute("message", "User Found.");
+        } else {
+            model.addAttribute("message", "User not found");
+        }
+
+        return "userToUpdate";
+    }
+
+    @PostMapping("/userUpdate/{userIdFromView}")
+    public String userUpdateSubmit(@PathVariable("userIdFromView") String id, Person person) {
+
+        personService.updateClient(id, person);
+
+        return "redirect:/home";
+    }
+
 }
